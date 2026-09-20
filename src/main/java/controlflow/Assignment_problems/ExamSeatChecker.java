89fe55a2d0c6dc731d6c1dd1600266b21549296a
VISHWAM KUMAR <vishwam.kr16418@gmail.com>
2026-09-20 12:44:50 +0530

    Create ExamSeatChecker.java

---
+++ src/main/java/controlflow/Assignment_problems/ExamSeatChecker.java
@@ -0,0 +1,16 @@
+public class ExamSeatChecker {
+    public static void checkDuplicateSeats(int[] seatNumbers) {
+        boolean found = false;
+        for (int i = 0; i < seatNumbers.length; i++) {
+            for (int j = i + 1; j < seatNumbers.length; j++) {
+                if (seatNumbers[i] == seatNumbers[j]) {
+                    System.out.println("Duplicate Seat Number Found: " + seatNumbers[i]);
+                    found = true;
+                }
+            }
+        }
+        if (!found) {
+            System.out.println("No Duplicate Seats Found");
+        }
+    }
+}
