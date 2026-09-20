73f5866aa454ff8592a7d447907af2c0ee9dd5aa
VISHWAM KUMAR <vishwam.kr16418@gmail.com>
2026-09-20 21:02:02 +0530

    Refactor maxSubArray method for improved readability

--- src/main/java/controlflow/Assignment_problems/MaximumSubarray.java
+++ src/main/java/controlflow/Assignment_problems/MaximumSubarray.java
@@ -1,13 +1,15 @@
 public class MaximumSubarray {
-    public static int maxSubArray(int[] nums) {
-        int maxSoFar = nums[0];
-        int currMax = nums[0];
-        
-        for (int i = 1; i < nums.length; i++) {
-            currMax = Math.max(nums[i], currMax + nums[i]);
-            maxSoFar = Math.max(maxSoFar, currMax);
-        }
-        
-        return maxSoFar;
-    }
+public static int maxSubArray(int[] nums) {
+int current=nums[0];
+int max=nums[0];
+for(int i=1;i<nums.length;i++){
+current=Math.max(nums[i],current+nums[i]);
+max=Math.max(max,current);
+}
+return max;
+}
+public static void main(String[] args) {
+int[] nums={-2,1,-3,4,-1,2,1,-5,4};
+System.out.println(maxSubArray(nums));
+}
 }
