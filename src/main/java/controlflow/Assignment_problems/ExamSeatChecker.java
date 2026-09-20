630a20f2aafdab81891132d4067da618cc64e54e
VISHWAM KUMAR <vishwam.kr16418@gmail.com>
2026-09-20 20:51:59 +0530

    Implement main method in ExamSeatChecker
    Added main method to test duplicate seat checking.

--- src/main/java/controlflow/Assignment_problems/ExamSeatChecker.java
+++ src/main/java/controlflow/Assignment_problems/ExamSeatChecker.java
@@ -1,6 +1,7 @@
 public class ExamSeatChecker {
     public static void checkDuplicateSeats(int[] seatNumbers) {
         boolean found = false;
+
         for (int i = 0; i < seatNumbers.length; i++) {
             for (int j = i + 1; j < seatNumbers.length; j++) {
                 if (seatNumbers[i] == seatNumbers[j]) {
@@ -12,5 +13,9 @@ public class ExamSeatChecker {
         if (!found) {
             System.out.println("No Duplicate Seats Found");
         }
+    }
+    public static void main(String[] args) {
+        int[] seats = {101, 102, 103, 102, 105};
+        checkDuplicateSeats(seats);
     }
 }
