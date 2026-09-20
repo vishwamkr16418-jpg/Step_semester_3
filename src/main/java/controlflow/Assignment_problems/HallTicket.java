43b33addfd29394534955e0d2d9319de9431fa78
VISHWAM KUMAR <vishwam.kr16418@gmail.com>
2026-09-20 19:02:45 +0530

    Create HallTicket.java

---
+++ src/main/java/controlflow/Assignment_problems/HallTicket.java
@@ -0,0 +1,9 @@
+public class HallTicket {
+    String studentName;
+    int seatNumber;
+
+    public HallTicket(String studentName, int seatNumber) {
+        this.studentName = studentName;
+        this.seatNumber = seatNumber;
+    }
+}
