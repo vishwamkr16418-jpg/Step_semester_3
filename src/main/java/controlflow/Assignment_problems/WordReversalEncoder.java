f74106fda6646185a24da2b208a384aac628139f
VISHWAM KUMAR <vishwam.kr16418@gmail.com>
2026-09-20 20:58:37 +0530

    Refactor reverseEachWord method for clarity
    Refactored the reverseEachWord method for better readability by removing unnecessary calls and simplifying the loop structure.

--- src/main/java/controlflow/Assignment_problems/WordReversalEncoder.java
+++ src/main/java/controlflow/Assignment_problems/WordReversalEncoder.java
@@ -1,14 +1,15 @@
 public class WordReversalEncoder {
-    public static String reverseEachWord(String sentence) {
-        String[] words = sentence.split(" ");
-        StringBuilder result = new StringBuilder();
-        for (int i = 0; i < words.length; i++) {
-            StringBuilder sb = new StringBuilder(words[i]);
-            result.append(sb.reverse().toString());
-            if (i < words.length - 1) {
-                result.append(" ");
-            }
-        }
-        return result.toString();
-    }
+public static String reverseEachWord(String sentence) {
+String[] words=sentence.split(" ");
+StringBuilder result=new StringBuilder();
+for(int i=0;i<words.length;i++){
+StringBuilder word=new StringBuilder(words[i]);
+result.append(word.reverse());
+if(i<words.length-1) result.append(" ");
+}
+return result.toString();
+}
+public static void main(String[] args) {
+System.out.println(reverseEachWord("hello club"));
+}
 }
