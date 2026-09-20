2d5b58744e4f0192daa738c4d6b907eebc66f969
VISHWAM KUMAR <vishwam.kr16418@gmail.com>
2026-09-20 21:19:16 +0530

    Refactor findMinMaxSpread method
    Refactored findMinMaxSpread method for improved readability and efficiency.

--- src/main/java/controlflow/Assignment_problems/TopPerformerTracker.java
+++ src/main/java/controlflow/Assignment_problems/TopPerformerTracker.java
@@ -1,16 +1,15 @@
 public class TopPerformerTracker {
-    public static String findMinMaxSpread(int[] scores) {
-        int min = scores[0];
-        int max = scores[0];
-        for (int i = 1; i < scores.length; i++) {
-            if (scores[i] < min) {
-                min = scores[i];
-            }
-            if (scores[i] > max) {
-                max = scores[i];
-            }
-        }
-        int spread = max - min;
-        return "Min: " + min + " Max: " + max + " Spread: " + spread;
-    }
+public static String findMinMaxSpread(int[] scores) {
+int min=scores[0];
+int max=scores[0];
+for(int i=1;i<scores.length;i++){
+if(scores[i]<min) min=scores[i];
+if(scores[i]>max) max=scores[i];
+}
+return "Min: "+min+" | Max: "+max+" | Spread: "+(max-min);
+}
+public static void main(String[] args) {
+int[] scores={45,82,79,90,33,90,61};
+System.out.println(findMinMaxSpread(scores));
+}
 }
