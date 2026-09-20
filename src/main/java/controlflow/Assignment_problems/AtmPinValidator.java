17f0ccde2df5f16743134300e517c29b9330f23e
VISHWAM KUMAR <vishwam.kr16418@gmail.com>
2026-09-20 20:58:02 +0530

    Update AtmPinValidator.java

--- src/main/java/controlflow/Assignment_problems/AtmPinValidator.java
+++ src/main/java/controlflow/Assignment_problems/AtmPinValidator.java
@@ -1,9 +1,9 @@
-public class AtmPinValidator {
-    public static void checkPinLength(String pin) {
-        if (pin.length() != 4) {
-            System.out.println("Invalid PIN must be exactly 4 digits.");
-        } else {
-            System.out.println("PIN length OK.");
-        }
-    }
+public class ATMPinValidator {
+public static void checkPinLength(String pin) {
+if(pin.length()!=4) System.out.println("Invalid PIN — must be exactly 4 digits.");
+else System.out.println("PIN length OK.");
+}
+public static void main(String[] args) {
+checkPinLength("4820");
+}
 }
