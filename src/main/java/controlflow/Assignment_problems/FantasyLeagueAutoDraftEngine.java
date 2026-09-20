405563b537c2a69521091fd9210ff3ca3945e54c
VISHWAM KUMAR <vishwam.kr16418@gmail.com>
2026-09-20 21:20:39 +0530

    Refactor FantasyLeagueAutoDraftEngine into FantasyDraftRanking

--- src/main/java/controlflow/Assignment_problems/FantasyLeagueAutoDraftEngine.java
+++ src/main/java/controlflow/Assignment_problems/FantasyLeagueAutoDraftEngine.java
@@ -1,53 +1,50 @@
 import java.util.Arrays;
-import java.util.ArrayList;
-import java.util.List;
-
-class Player implements Comparable<Player> {
-    String name;
-    int matchesPlayed;
-    double battingAverage;
-    boolean injured;
-
-    public Player(String name, int matchesPlayed, double battingAverage, boolean injured) {
-        this.name = name;
-        this.matchesPlayed = matchesPlayed;
-        this.battingAverage = battingAverage;
-        this.injured = injured;
-    }
-
-    @Override
-    public int compareTo(Player other) {
-        return Double.compare(other.battingAverage, this.battingAverage);
-    }
+public class FantasyDraftRanking {
+static class Player implements Comparable<Player> {
+String name;
+int matchesPlayed;
+double battingAverage;
+boolean injured;
+public Player(String name,int matchesPlayed,double battingAverage,boolean injured) {
+this.name=name;
+this.matchesPlayed=matchesPlayed;
+this.battingAverage=battingAverage;
+this.injured=injured;
 }
-
-public class FantasyLeagueAutoDraftEngine {
-    public static boolean isDraftable(int matchesPlayed) {
-        return matchesPlayed >= 10;
-    }
-
-    public static boolean isDraftable(int matchesPlayed, boolean injured) {
-        return matchesPlayed >= 5 && !injured;
-    }
-
-    public static String draftAndRank(Player[] players) {
-        List<Player> draftableList = new ArrayList<>();
-        for (Player p : players) {
-            if (isDraftable(p.matchesPlayed) || isDraftable(p.matchesPlayed, p.injured)) {
-                draftableList.add(p);
-            }
-        }
-        
-        Player[] draftableArray = draftableList.toArray(new Player[0]);
-        Arrays.sort(draftableArray);
-        
-        StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < draftableArray.length; i++) {
-            sb.append((i + 1)).append(". ").append(draftableArray[i].name);
-            if (i < draftableArray.length - 1) {
-                sb.append(" ");
-            }
-        }
-        return sb.toString();
-    }
+public int compareTo(Player other) {
+return Double.compare(other.battingAverage,this.battingAverage);
+}
+}
+static boolean isDraftable(int matchesPlayed) {
+return matchesPlayed>=10;
+}
+static boolean isDraftable(int matchesPlayed,boolean injured) {
+return matchesPlayed>=5&&!injured;
+}
+static String draftAndRank(Player[] players) {
+Player[] temp=new Player[players.length];
+int count=0;
+for(Player p:players){
+if(isDraftable(p.matchesPlayed)||isDraftable(p.matchesPlayed,p.injured)){
+temp[count++]=p;
+}
+}
+Player[] draftable=Arrays.copyOf(temp,count);
+Arrays.sort(draftable);
+StringBuilder result=new StringBuilder();
+for(int i=0;i<draftable.length;i++){
+if(i>0) result.append(" | ");
+result.append(i+1).append(". ").append(draftable[i].name);
+}
+return result.toString();
+}
+public static void main(String[] args) {
+Player[] players={
+new Player("Virat",15,48.0,false),
+new Player("Rahul",7,55.0,false),
+new Player("Sameer",3,60.0,false),
+new Player("Dev",12,20.0,true)
+};
+System.out.println(draftAndRank(players));
+}
 }
