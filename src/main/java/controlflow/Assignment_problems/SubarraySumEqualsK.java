336396165835c4426ed263ac372736eb93cfd644
VISHWAM KUMAR <vishwam.kr16418@gmail.com>
2026-09-20 21:03:47 +0530

    Refactor SubarraySumEqualsK to SubarraySumK

--- src/main/java/controlflow/Assignment_problems/SubarraySumEqualsK.java
+++ src/main/java/controlflow/Assignment_problems/SubarraySumEqualsK.java
@@ -1,21 +1,19 @@
-import java.util.HashMap;
-import java.util.Map;
-
-public class SubarraySumEqualsK {
-    public static int subarraySum(int[] nums, int k) {
-        int count = 0;
-        int currentSum = 0;
-        Map<Integer, Integer> prefixSumMap = new HashMap<>();
-        prefixSumMap.put(0, 1);
-        
-        for (int num : nums) {
-            currentSum += num;
-            if (prefixSumMap.containsKey(currentSum - k)) {
-                count += prefixSumMap.get(currentSum - k);
-            }
-            prefixSumMap.put(currentSum, prefixSumMap.getOrDefault(currentSum, 0) + 1);
-        }
-        
-        return count;
-    }
+import java.util.*;
+public class SubarraySumK {
+public static int subarraySum(int[] nums,int k) {
+HashMap<Integer,Integer> map=new HashMap<>();
+map.put(0,1);
+int sum=0;
+int count=0;
+for(int num:nums){
+sum+=num;
+count+=map.getOrDefault(sum-k,0);
+map.put(sum,map.getOrDefault(sum,0)+1);
+}
+return count;
+}
+public static void main(String[] args) {
+int[] nums={1,1,1};
+System.out.println(subarraySum(nums,2));
+}
 }
