6bb9d15818d1959eff96bc8d3bf36f8b110b78f1
VISHWAM KUMAR <vishwam.kr16418@gmail.com>
2026-09-20 21:28:24 +0530

    Refactor Employee class into EmployeeProfile class

--- src/main/java/controlflow/Assignment_problems/Employee.java
+++ src/main/java/controlflow/Assignment_problems/Employee.java
@@ -1,30 +1,27 @@
-public class Employee {
-    String empId;
-    String empName;
-    double salary;
-    boolean isIntern;
-    static String companyName = "Bright Horizon Technologies";
-    static int employeeCount = 0;
-
-    public Employee(String empId, String empName, double salary) {
-        this.empId = empId;
-        this.empName = empName;
-        this.salary = salary;
-        this.isIntern = false;
-        employeeCount++;
-    }
-
-    public Employee(String empId, String empName) {
-        this(empId, empName, 0.0);
-        this.isIntern = true;
-    }
-
-    public void printProfile() {
-        System.out.println(empId + " -| " + empName + " | Rs " + salary + " | Intern: " + isIntern);
-    }
-
-    public static void printCompanyInfo() {
-        System.out.println(companyName);
-        System.out.println("Employees on record: " + employeeCount);
-    }
+public class EmployeeProfile {
+static class Employee {
+String empId;
+String empName;
+double salary;
+boolean isIntern;
+public Employee(String empId,String empName,double salary) {
+this.empId=empId;
+this.empName=empName;
+this.salary=salary;
+this.isIntern=false;
+}
+public Employee(String empId,String empName) {
+this(empId,empName,0);
+this.isIntern=true;
+}
+public void printProfile() {
+System.out.println(empId+" | "+empName+" | Rs "+salary+" | Intern: "+isIntern);
+}
+}
+public static void main(String[] args) {
+Employee e1=new Employee("E-101","Divya",65000);
+Employee e2=new Employee("E-102","Arjun");
+e1.printProfile();
+e2.printProfile();
+}
 }
