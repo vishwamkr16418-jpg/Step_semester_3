c5bbaa415f0c839c5fd251c7ac806be378290ec7
VISHWAM KUMAR <vishwam.kr16418@gmail.com>
2026-09-20 21:03:09 +0530

    Refactor threeSum method and improve output handling

--- src/main/java/controlflow/Assignment_problems/ThreeSum.java
+++ src/main/java/controlflow/Assignment_problems/ThreeSum.java
@@ -1,40 +1,29 @@
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-
+import java.util.*;
 public class ThreeSum {
-    public static int[][] threeSum(int[] nums) {
-        Arrays.sort(nums);
-        List<int[]> result = new ArrayList<>();
-        
-        for (int i = 0; i < nums.length - 2; i++) {
-            if (i > 0 && nums[i] == nums[i - 1]) continue;
-            
-            int left = i + 1;
-            int right = nums.length - 1;
-            
-            while (left < right) {
-                int sum = nums[i] + nums[left] + nums[right];
-                
-                if (sum == 0) {
-                    result.add(new int[]{nums[i], nums[left], nums[right]});
-                    
-                    while (left < right && nums[left] == nums[left + 1]) left++;
-                    while (left < right && nums[right] == nums[right - 1]) right--;
-                    
-                    left++;
-                    right--;
-                } else if (sum < 0) {
-                    left++;
-                } else {
-                    right--;
-                }
-            }
-        }
-        int[][] output = new int[result.size()][];
-        for (int i = 0; i < result.size(); i++) {
-            output[i] = result.get(i);
-        }
-        return output;
-    }
+public static int[][] threeSum(int[] nums) {
+Arrays.sort(nums);
+ArrayList<int[]> list=new ArrayList<>();
+for(int i=0;i<nums.length-2;i++){
+if(i>0&&nums[i]==nums[i-1]) continue;
+int left=i+1;
+int right=nums.length-1;
+while(left<right){
+int sum=nums[i]+nums[left]+nums[right];
+if(sum==0){
+list.add(new int[]{nums[i],nums[left],nums[right]});
+while(left<right&&nums[left]==nums[left+1]) left++;
+while(left<right&&nums[right]==nums[right-1]) right--;
+left++;
+right--;
+}else if(sum<0) left++;
+else right--;
+}
+}
+return list.toArray(new int[list.size()][]);
+}
+public static void main(String[] args) {
+int[] nums={-1,0,1,2,-1,-4};
+int[][] result=threeSum(nums);
+for(int[] x:result) System.out.println(Arrays.toString(x));
+}
 }
