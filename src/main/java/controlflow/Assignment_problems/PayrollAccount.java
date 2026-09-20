133ed8c9d74557433ef04633331f1ede416b529c
VISHWAM KUMAR <vishwam.kr16418@gmail.com>
2026-09-20 21:27:40 +0530

    Refactor PayrollAccount class implementation
    Refactored PayrollAccount class to improve code clarity and consistency in messages.

--- src/main/java/controlflow/Assignment_problems/PayrollAccount.java
+++ src/main/java/controlflow/Assignment_problems/PayrollAccount.java
@@ -1,36 +1,36 @@
 public class PayrollAccount {
-    private double basicSalary;
-    private double bonus;
-
-    public PayrollAccount(double initialSalary) {
-        if (initialSalary < 0) {
-            this.basicSalary = 0;
-            System.out.println("Warning: Negative salary provided. Set to 0.");
-        } else {
-            this.basicSalary = initialSalary;
-        }
-        this.bonus = 0;
-    }
-
-    public void creditBonus(double amount) {
-        if (amount <= 0) {
-            System.out.println("Invalid bonus amount.");
-        } else {
-            this.bonus += amount;
-            System.out.println("Bonus credited: Rs " + amount);
-        }
-    }
-
-    public void deductTax(double percent) {
-        if (percent < 0 || percent > 100) {
-            System.out.println("Invalid tax percentage.");
-        } else {
-            this.basicSalary -= this.basicSalary * (percent / 100.0);
-            System.out.println("Tax deducted: " + (int)percent + "%");
-        }
-    }
-
-    public double getNetSalary() {
-        return basicSalary + bonus;
-    }
+private double basicSalary;
+private double bonus;
+public PayrollAccount(double basicSalary) {
+if(basicSalary<0){
+this.basicSalary=0;
+System.out.println("Warning: Negative salary not allowed. Salary set to Rs 0.0");
+}else{
+this.basicSalary=basicSalary;
+}
+}
+public void creditBonus(double amount) {
+if(amount<=0) System.out.println("Invalid bonus amount");
+else{
+bonus+=amount;
+System.out.println("Bonus credited: Rs "+amount);
+}
+}
+public void deductTax(double percent) {
+if(percent<0||percent>100){
+System.out.println("Invalid tax percentage");
+}else{
+basicSalary-=basicSalary*percent/100;
+System.out.println("Tax deducted: "+percent+"%");
+}
+}
+public double getNetSalary() {
+return basicSalary+bonus;
+}
+public static void main(String[] args) {
+PayrollAccount account=new PayrollAccount(50000);
+account.creditBonus(5000);
+account.deductTax(10);
+System.out.println("Net salary: Rs "+account.getNetSalary());
+}
 }
