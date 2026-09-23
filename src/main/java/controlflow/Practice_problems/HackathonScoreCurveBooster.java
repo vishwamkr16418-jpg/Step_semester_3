4f47799d458da7942c06f405002d27eab4d421e7
VISHWAM KUMAR <vishwam.kr16418@gmail.com>
2026-09-24 00:05:59 +0530

    Add HackathonScoreCurveBooster to curve scores

---
+++ src/main/java/controlflow/Practice_problems/HackathonScoreCurveBooster.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class HackathonScoreCurveBooster{
+static void curveScores(int[] scores,int bonus){
+for(int i=0;i<scores.length;i++)scores[i]+=bonus;
+}
+public static void main(String[] args){
+int[] scores={70,85,60};
+curveScores(scores,10);
+System.out.println(Arrays.toString(scores));
+}
+}
