292cf5e71372d295a2903f070b0990ee2fa6c3df
VISHWAM KUMAR <vishwam.kr16418@gmail.com>
2026-09-23 23:56:58 +0530

    Add ContainsDuplicate class with duplicate check method
    Implement a method to check for duplicate integers in an array.

---
+++ src/main/java/controlflow/Practice_problems/ContainsDuplicate.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class ContainsDuplicate{
+public static boolean containsDuplicate(int[] nums){
+for(int i=0;i<nums.length;i++){
+for(int j=i+1;j<nums.length;j++){
+if(nums[i]==nums[j])return true;
+}
+}
+return false;
+}
+public static void main(String[] args){
+Scanner sc=new Scanner(System.in);
+int n=sc.nextInt();
+int[] nums=new int[n];
+for(int i=0;i<n;i++)nums[i]=sc.nextInt();
+System.out.println(containsDuplicate(nums));
+}
+}
